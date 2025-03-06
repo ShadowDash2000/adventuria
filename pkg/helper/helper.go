7636f7a821ab086820725d97024f129f6393308e
shadod <scout0501@gmail.com>
2025-03-07 02:15:32 +0300

    fix(helper): check if slice len is greater than 1

--- pkg/helper/helper.go
+++ pkg/helper/helper.go
@@ -3,5 +3,9 @@ package helper
 import "math/rand"
 
 func RandomItemFromSlice[T any](slice []T) T {
-	return slice[rand.Intn(len(slice)-1)]
+	n := 0
+	if len(slice) > 1 {
+		n = rand.Intn(len(slice) - 1)
+	}
+	return slice[n]
 }
