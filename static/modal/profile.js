c0f064f9644f2cf5de00303099aefcd1e7fc0533
shadod <scout0501@gmail.com>
2025-03-08 22:45:46 +0300

    feat: added current cell info into profile

--- static/modal/profile.js
+++ static/modal/profile.js
@@ -6,6 +6,10 @@ const profileName = profileModal.querySelector('h2');
 const profileAvatar = profileModal.querySelector('.profile-modal img');
 const profileDescription = profileModal.querySelector('.profile-modal .profile-modal__description');
 
+const profileCellImg = profileModal.querySelector('.current-cell img');
+const profileCellName = profileModal.querySelector('.current-cell .profile-modal__name');
+const profileCellDescription = profileModal.querySelector('.current-cell .profile-modal__description');
+
 document.addEventListener('profile.open', (e) => {
     putUserInfoToProfile(e.detail.userId);
     app.modal.open('profile', {
@@ -16,9 +20,14 @@ document.addEventListener('profile.open', (e) => {
 
 function putUserInfoToProfile(userId) {
     const user = app.users.getById(userId);
+    const currentCell = app.users.getUserCurrentCell(userId);
 
     profileName.innerText = `ПРОФИЛЬ ${user.name}`;
     profileAvatar.src = Helper.getFile('avatar', user);
     profileAvatar.style.borderColor = user.color;
     profileDescription.innerHTML = user.description;
+
+    profileCellImg.src = Helper.getFile('icon', currentCell, {'thumb': '250x0'});
+    profileCellName.innerText = currentCell.name;
+    profileCellDescription.innerHTML = currentCell.description;
 }
